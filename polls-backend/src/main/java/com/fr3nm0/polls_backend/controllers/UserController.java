f2f817bdffc698e855f589f0172a256509407d35
fr3nm0 <efren.morales.flores@gmail.com>
2024-06-27 19:00:57 -0600

    [feat] Configuracion inicial

---
+++ polls-backend/src/main/java/com/fr3nm0/polls_backend/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.fr3nm0.polls_backend.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping
+    public String createUser(){
+        return "Create user";
+    }
+}
---
+++ polls-backend/src/test/java/com/fr3nm0/polls_backend/PollsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr3nm0.polls_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
